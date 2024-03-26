5a0a76f589fbdb6d3676a483802d9ec3266c06cd
Kartbllansh <movolovo93@gmail.com>
2024-03-26 16:57:39 +0300

    ccvfcvgd

---
+++ src/ForGit.java
@@ -0,0 +1,19 @@
+public class ForGit {
+    public ForGit() {
+    }
+
+    @Override
+    public String toString() {
+        return "ForGit{}";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
