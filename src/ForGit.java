8687e797c4248f3b318552671ae7812ddcbfa2e5
Kartbllansh <movolovo93@gmail.com>
2024-03-27 16:32:47 +0300

    fff

--- src/ForGit.java
+++ src/ForGit.java
@@ -13,6 +13,11 @@ public class ForGit {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
     public int hashCode() {
         return super.hashCode();
     }
